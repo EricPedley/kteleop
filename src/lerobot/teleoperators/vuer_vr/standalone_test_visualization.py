5d416c51abb723721297c13a16f805a0e16aa46c
Eric Pedley <ericpedley@gmail.com>
2025-08-21 20:05:58 -0700

    got ik to run relatively fast

--- src/lerobot/teleoperators/vuer_vr/standalone_test_visualization.py
+++ src/lerobot/teleoperators/vuer_vr/standalone_test_visualization.py
@@ -12,7 +12,7 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 from scipy.optimize import least_squares
 from line_profiler import profile
-import ikpy
+import ikpy.chain
 
 def fast_mat_inv(mat):
     ret = np.eye(4)
@@ -48,14 +48,24 @@ print(right_hand_robot.actuated_joint_names)
 left_hand_robot = load_urdf_with_absolute_paths('assets/inspire_hand/inspire_hand_left.urdf', 'assets/inspire_hand')
 kbot_robot = load_urdf_with_absolute_paths('assets/kbot/robot.urdf', 'assets/kbot')
 
+right_chain = ikpy.chain.Chain.from_urdf_file(
+    f"{file_absolute_parent}/assets/kbot/robot.urdf",
+    base_elements=['Torso_Side_Right']  # Start from the torso and let ikpy auto-discover
+)
+
+left_chain = ikpy.chain.Chain.from_urdf_file(
+    f"{file_absolute_parent}/assets/kbot/robot.urdf",
+    base_elements=['Torso_Side_Left']  # Start from the torso and let ikpy auto-discover
+)
+
 app = Vuer(static_root=Path(__file__).parent / "assets")
 
 head_matrix_shared = np.zeros((4, 4), dtype=np.float32)
 left_hand_shared = np.zeros((4, 4), dtype=np.float32)
-left_landmarks_shared = np.zeros((25,4), dtype=np.float32)
+left_landmarks_shared = np.zeros((25,4,4), dtype=np.float32)
 
 right_hand_shared = np.zeros((4, 4), dtype=np.float32)
-right_landmarks_shared = np.zeros((25,4), dtype=np.float32)
+right_landmarks_shared = np.zeros((25,4,4), dtype=np.float32)
 
 vuer_to_urdf_mat = Rotation.from_euler('xz', (90, 90), degrees=True).as_matrix()
 prev_joint_angles = np.zeros(len(right_hand_robot.actuated_joint_names), dtype=np.float32)
@@ -135,20 +145,27 @@ async def hand_move_handler(event, session):
             left_mat_raw = event.value['left'] # 400-long float array, 25 4x4 matrices
             left_mat_numpy = np.array(left_mat_raw, dtype=np.float32).reshape(25, 4, 4)
             left_hand_shared[:] = left_mat_numpy[0].T  # Use the first matrix as the hand pose
-            left_landmarks_shared[:] = left_mat_numpy[:, 3]
+            left_landmarks_shared[:] = left_mat_numpy
 
         if 'rightState' in event.value and event.value['rightState']:
             right_mat_raw = event.value['right']
             right_mat_numpy = np.array(right_mat_raw, dtype=np.float32).reshape(25, 4, 4)
             right_hand_shared[:] = right_mat_numpy[0].T  # Use the first matrix as the hand pose
-            right_landmarks_shared[:] = right_mat_numpy[:, 3]
+            right_landmarks_shared[:] = right_mat_numpy
 
     right_tips = right_landmarks_shared[tip_indices]
     rel_right_tips = right_tips @ fast_mat_inv(right_hand_shared)
-    right_tips_urdf_frame = rel_right_tips[:,:3] @ vuer_to_urdf_mat.T
+    right_tip_x_angles = [
+        Rotation.from_matrix(tip_mat[:3,:3]).as_euler('xyz', degrees=False)[0]
+        for tip_mat in rel_right_tips
+    ]
 
-    # right_hand_joints = right_hand_inverse_kinematics(right_tips_urdf_frame)
-    right_hand_joints = np.zeros(6)
+    right_tip_x_angles.append(0.0)
+
+    # right_tips_urdf_frame = rel_right_tips[:,:3] @ vuer_to_urdf_mat.T
+
+    # # right_hand_joints = right_hand_inverse_kinematics(right_tips_urdf_frame)
+    # right_hand_joints = np.zeros(6)
 
     right_hand_transformed = right_hand_shared.copy()
     right_hand_transformed[:3, :3] = right_hand_transformed[:3, :3] @ vuer_to_urdf_mat
@@ -157,7 +174,7 @@ async def hand_move_handler(event, session):
 
     session.upsert @ Urdf(
         src="https://10.33.12.199/static/inspire_hand/inspire_hand_right.urdf",
-        jointValues=dict(zip(right_hand_robot.actuated_joint_names, right_hand_joints, strict=True)),
+        jointValues=dict(zip(right_hand_robot.actuated_joint_names, right_tip_x_angles, strict=True)),
         matrix = right_hand_transformed.T.flatten().tolist(),
         scale=1,
         key="right_hand",
@@ -165,24 +182,45 @@ async def hand_move_handler(event, session):
 
 
 
-    left_hand_transformed = left_hand_shared.copy()
-    left_hand_transformed[:3, :3] = left_hand_transformed[:3, :3] @ vuer_to_urdf_mat
-    session.upsert @ Urdf(
-        src="https://10.33.12.199/static/inspire_hand/inspire_hand_left.urdf",
-        jointValues={k: 0.0 for k in left_hand_robot.actuated_joint_names},
-        matrix = left_hand_transformed.T.flatten().tolist(),
-        scale=1,
-        key="left_hand",
-    )
 
     kbot_position = np.array([0,1 if head_height is None else head_height+0.5,0])
-    kbot_rot_mat = Rotation.from_euler('xy', [-90, 90], degrees=True).as_matrix()
+    kbot_rot_mat = Rotation.from_euler('xy', [-90, -90], degrees=True).as_matrix()
     kbot_matrix = np.eye(4)
     kbot_matrix[:3, :3] = kbot_rot_mat
     kbot_matrix[:3, 3] = kbot_position
+
+    def transform_to_kbot_frame(vuer_frame):
+        """Transform a Vuer frame to the Kbot frame."""
+        transform_mat = np.eye(4)
+        transform_mat[:3, :3] = Rotation.from_euler('xy', [180,180], degrees=True).as_matrix()
+        return transform_mat @ vuer_frame
+
+    right_hand_kbot_frame = transform_to_kbot_frame(right_hand_shared)[:3,3]
+    left_hand_kbot_frame = transform_to_kbot_frame(left_hand_shared)[:3,3]
+
+    right_arm_joints = right_chain.inverse_kinematics(
+        target_position=right_hand_kbot_frame,
+    )
+
+
+    left_arm_joints = left_chain.inverse_kinematics(
+        target_position=left_hand_kbot_frame,
+    )
+
+    # left_arm_joints = np.zeros(6)
+    # right_arm_joints = np.zeros(6)
+
+    
+
+    joint_values = {}
+    for i, link in enumerate(left_chain.links):
+        joint_values[link.name] = left_arm_joints[i]
+    for i, link in enumerate(right_chain.links):
+        joint_values[link.name] = right_arm_joints[i]
+
     session.upsert @ Urdf(
         src="https://10.33.12.199/static/kbot/robot.urdf",
-        jointValues=np.zeros(len(kbot_robot.actuated_joint_names)).tolist(),
+        jointValues=joint_values,
         matrix=kbot_matrix.T.flatten().tolist(),
         scale=1,
         key="kbot",
@@ -227,6 +265,15 @@ async def main(sess: VuerSession):
 
         # Update all three robots
 
+        left_hand_transformed = left_hand_shared.copy()
+        left_hand_transformed[:3, :3] = left_hand_transformed[:3, :3] @ vuer_to_urdf_mat
+        sess.upsert @ Urdf(
+            src="https://10.33.12.199/static/inspire_hand/inspire_hand_left.urdf",
+            jointValues={k: 0.0 for k in left_hand_robot.actuated_joint_names},
+            matrix = left_hand_transformed.T.flatten().tolist(),
+            scale=1,
+            key="left_hand",
+        )
 
 
 
